c4172dcdf25a6036124784d22042b16c599dafc1
[AloneAsif] <[asifleon3@gmail.com]>
2024-03-11 23:38:49 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,7 @@
+// Question no 34       pizza
+
+let fav_pizza: string[] = ['pineapple','Pepperoni','Sausage']
+for(let pizza of fav_pizza){
+    console.log(`ny favorite pizza is ${pizza}`)
+}
+console.log(`i really love pizza \nbecause pizza is so delicious.\nmost i love pepperoni pizza.`)
